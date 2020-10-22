829995eb86eae25f498528ddeb9b18847e1afff2
Teddylongs <eifemade@gmail.com>
2020-10-21 21:47:42 -0400

    started meet tope section

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,26 @@ import Layout from '../components/layout'
 import '../styles/main.css'
 
 
+import meetTopeImg from '../images/meet-tope-image.png'
+import meetTopeTxt from '../images/meet-tope-text.png'
+import { CTA } from "../components/ButtonElements"
+
 const IndexPage = () => (
  <>
- <Layout />
+ <Layout>
+     <section id='meet-tope'>
+         <div className></div>
+         <div className='content-left'>
+             <img src={meetTopeImg} alt=''/>
+         </div>
+         <div className='content-right'>
+            <img src={meetTopeTxt} alt=''/>
+
+                <CTA style={{'display': 'block', 'margin': 'auto'}}>Learn More</CTA>
+         </div>
+
+     </section>
+ </Layout>
  </>
 )
 
